04e652ae5c4fc1e196a95d495709b6dd88e46cc4
Andrei Mihai Ciobanu <89391344+amcbn06@users.noreply.github.com>
2025-01-25 15:27:52 +0200

    Update union_find.cpp

--- Data Structures/union_find.cpp
+++ Data Structures/union_find.cpp
@@ -1,36 +1,30 @@
-// Works with nodes in range [1, size]
-struct union_find {
-    vector<int> parent, rank;
-    int size, components;
-    union_find(int size = 0) {
-        this->resize(size);
-    }
-    void resize(int size) {
-        this->size = size;
-        this->components = size;
-        parent.resize(size + 1);
-        for (int i = 1; i <= size; ++i) {
-            parent[i] = i;
-        }
-        rank.assign(size + 1, 1);
-    }
-    int find(int x) {
-        return parent[x] == x ? x : (parent[x] = find(parent[x]));
-    }
-    void merge(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) {
-            return;
-        }
-        if (rank[x] < rank[y]) {
-            swap(x, y);
-        }
-        parent[y] = x;
-        rank[x] += rank[y];
-        rank[y] = 0;
-    }
-    bool connected(int x, int y) {
-        return find(x) == find(y);
-    }
+// Works with nodes in range [0, __size)
+struct UnionFind {
+	vector<int> parent, size;
+	UnionFind(int __size) {
+		parent.resize(__size);
+		size.resize(__size);
+		for (int i = 0; i < __size; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+	int find(int x) {
+		return parent[x] == x ? x : (parent[x] = find(parent[x]));
+	}
+	void merge(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y) {
+			return;
+		}
+		if (size[x] < size[y]) {
+			swap(x, y);
+		}
+		parent[y] = x;
+		size[x] += size[y];
+	}
+	bool connected(int x, int y) {
+		return find(x) == find(y);
+	}
 };
