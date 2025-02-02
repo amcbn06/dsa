d40d0cac5864581af35c35b30891d698a224d112
Andrei Mihai Ciobanu <89391344+amcbn06@users.noreply.github.com>
2025-02-02 13:49:48 +0100

    Update maxmatching.cpp

--- Flows and Matchings/maxmatching.cpp
+++ Flows and Matchings/maxmatching.cpp
@@ -9,20 +9,13 @@ struct MaxMatching {
         g.resize(V);
     }
     void addEdge(int u, int v) {
+        assert(0 <= u && u < V);
+        assert(0 <= v && v < V);
         g[u].push_back(v);
         g[v].push_back(u);
     }
     int matching() {
         vector<bool> vis(V);
-        function<void(int)> colorGraph = [&](int u) {
-            vis[u] = true;
-            for (auto& v : g[u]) {
-                if (!vis[v]) {
-                    color[v] = color[u] ^ 1;
-                    colorGraph(v);
-                }
-            }
-        };
         function<bool(int)> findPath = [&](int u) {
             vis[u] = true;
             for (auto& v : g[u]) {
@@ -34,11 +27,6 @@ struct MaxMatching {
             }
             return false;
             };
-        for (int i = 0; i < V; ++i) {
-            if (!vis[i]) {
-                colorGraph(i);
-            }
-        }
         int m = 0, ok = true;
         while (ok) {
             ok = false;
@@ -51,5 +39,36 @@ struct MaxMatching {
             }
         }
         return m;
+    }
+    vector<int> minimumVertexCover() {
+        vector<int> mvc(V);
+        function<void(int)> dfs = [&](int u) {
+            mvc[u] = true;
+            for (auto& v : g[u]) {
+                if (!mvc[v]) {
+                    mvc[v] = true;
+                    if (match[v] != -1) {
+                        dfs(match[v]);
+                    }
+                }
+            }
+            };
+        for (int i = 0; i < V; ++i) {
+            if (color[i] && match[i] == -1 && !mvc[i]) {
+                dfs(i);
+            }
+        }
+        vector<int> nodes;
+        for (int i = 0; i < V; ++i) {
+            if (color[i]) {
+                if (!mvc[i]) {
+                    nodes.push_back(i);
+                }
+            }
+            else if (mvc[i]) {
+                nodes.push_back(i);
+            }
+        }
+        return nodes;
     }
 };
