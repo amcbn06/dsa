283f3b09f8d6c0114c363faf1ddb29bd78032392
Andrei Mihai Ciobanu <89391344+amcbn06@users.noreply.github.com>
2025-02-18 13:09:24 +0200

    Update fenwick.cpp

--- Data Structures/Fenwick tree/sum_fenwick.cpp
+++ Data Structures/Fenwick tree/sum_fenwick.cpp
@@ -1,22 +1,17 @@
 // Works with indicies in range [1, size]
 template<typename T>
-struct sum_fenwick {
+struct FenwickTree {
     vector<T> table;
     int size;
-    sum_fenwick(int size = 0) {
-        this->resize(size);
-    }
-    void resize(int size) {
+    FenwickTree(int size = 0) {
         this->size = size;
         table.assign(size + 1, 0);
     }
-    // a[i] += x
     void add(int i, T x) {
         for (; i <= size; i += i & -i) {
             table[i] += x;
         }
     }
-    // sum(a[1...i])
     T query(int i) {
         T sum = 0;
         for (; i > 0; i -= i & -i) {
@@ -24,10 +19,7 @@ struct sum_fenwick {
         }
         return sum;
     }
-    // sum(a[i...j])
     T query(int i, int j) {
         return query(j) - query(i - 1);
     }
 };
-
-using fenwick = sum_fenwick<int>; 
